3215db659a1317f983e27686b61a0a85e35c02ab
LAPTOP-I3HRPI1D\姜弘 <1586676476@qq.com>
2017-05-08 16:26:50 +0800

    123

---
+++ app/src/main/java/com/example/medical/DoctorFragment.java
@@ -0,0 +1,21 @@
+package com.example.medical;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ${liyan} on 2017/5/8.
+ */
+
+public class DoctorFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_doctor,container,false);
+        return view;
+    }
+}
